fcb62ae1b1e488c1179e51f4e436957a0585d336
Kayli Garwood <kgarwood@uncc.edu>
2020-10-18 16:45:45 -0400

    Added Settings functionality

--- server.js
+++ server.js
@@ -67,6 +67,13 @@ app.get('/api/dashboard', jwtMW, (req, res) => {
     });
 });
 
+app.get('/api/settings', jwtMW, (req, res) => {
+    res.json({
+        success: true,
+        myContent: 'This is the settings page!!'
+    });
+});
+
 app.get('/api/prices', jwtMW, (req, res) => {
     res.json({
         success: true,
